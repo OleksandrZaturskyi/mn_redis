564608b1bc613435e66f9185423874fffed9ed38
aleksandr <zaturskyi.aleksandr@gmail.com>
2022-03-26 16:15:29 +0200

    shared state

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,72 @@
-use mini_redis::{Command, Connection, Frame};
-use std::collections::HashMap;
-use tokio::net::{TcpListener, TcpStream};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6380").await.unwrap();
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
         tokio::spawn(async move {
-            process(socket).await;
+            handlers::process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    let mut db = HashMap::new();
+mod handlers {
+    use crate::db::Db;
+    use mini_redis::{Command, Connection, Frame};
+    use tokio::net::TcpStream;
 
-    let mut connection = Connection::new(socket);
+    pub async fn process(socket: TcpStream, db: Db) {
+        let mut connection = Connection::new(socket);
 
-    while let Some(frame) = connection.read_frame().await.unwrap() {
-        let response = match Command::from_frame(frame).unwrap() {
-            Command::Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
-                Frame::Simple("OK".to_string())
-            }
-            Command::Get(cmd) => match db.get(cmd.key()) {
-                Some(v) => Frame::Bulk(v.clone().into()),
-                None => Frame::Null,
-            },
-            cmd => panic!("unimplemented {cmd:?}"),
-        };
+        while let Some(frame) = connection.read_frame().await.unwrap() {
+            let response = match Command::from_frame(frame).unwrap() {
+                Command::Set(cmd) => {
+                    db.lock()
+                        .unwrap()
+                        .insert(cmd.key().to_string(), cmd.value().clone());
+                    Frame::Simple("OK".to_string())
+                }
+                Command::Get(cmd) => match db.lock().unwrap().get(cmd.key()) {
+                    Some(v) => Frame::Bulk(v.clone().into()),
+                    None => Frame::Null,
+                },
+                cmd => panic!("unimplemented {cmd:?}"),
+            };
 
-        connection.write_frame(&response).await.unwrap();
+            connection.write_frame(&response).await.unwrap();
+        }
     }
+}
+
+mod db {
+    use std::{
+        collections::HashMap,
+        sync::{Arc, Mutex},
+    };
+
+    use bytes::Bytes;
+
+    pub type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+    
+    // pub type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
+
+    // fn new_sharded_db(num_shards: usize) -> ShardedDb {
+    //     let mut db = Vec::with_capacity(num_shards);
+    //     for _ in 0..num_shards {
+    //         db.push(Mutex::new(HashMap::new()))
+    //     }
+    //     Arc::new(db)
+    // }
 }
