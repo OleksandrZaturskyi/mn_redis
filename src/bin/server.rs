55ecdd33c20a90be02626d3137bdf6ca3122a3cd
aleksandr <zaturskyi.aleksandr@gmail.com>
2022-03-28 15:46:19 +0300

    framing

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,5 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
-
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -23,8 +20,9 @@ async fn main() {
 }
 
 mod handlers {
+    use crate::connection::Connection;
     use crate::db::Db;
-    use mini_redis::{Command, Connection, Frame};
+    use mini_redis::{Command, Frame};
     use tokio::net::TcpStream;
 
     pub async fn process(socket: TcpStream, db: Db) {
@@ -59,7 +57,7 @@ mod db {
     use bytes::Bytes;
 
     pub type Db = Arc<Mutex<HashMap<String, Bytes>>>;
-    
+
     // pub type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
 
     // fn new_sharded_db(num_shards: usize) -> ShardedDb {
@@ -69,4 +67,111 @@ mod db {
     //     }
     //     Arc::new(db)
     // }
+}
+
+mod connection {
+    use bytes::{Buf, BytesMut};
+    use mini_redis::frame::Error::Incomplete;
+    use mini_redis::Frame;
+    use std::io::Cursor;
+    use std::io::Write;
+    use tokio::io::{self, AsyncReadExt, AsyncWriteExt, BufWriter};
+    use tokio::net::TcpStream;
+
+    pub struct Connection {
+        stream: BufWriter<TcpStream>,
+        buffer: BytesMut,
+    }
+
+    impl Connection {
+        pub fn new(stream: TcpStream) -> Self {
+            Self {
+                stream: BufWriter::new(stream),
+                buffer: BytesMut::with_capacity(4096),
+            }
+        }
+
+        pub async fn read_frame(&mut self) -> mini_redis::Result<Option<Frame>> {
+            loop {
+                if let Some(frame) = self.parse_frame()? {
+                    return Ok(Some(frame));
+                }
+
+                if 0 == self.stream.read_buf(&mut self.buffer).await? {
+                    if self.buffer.is_empty() {
+                        return Ok(None);
+                    } else {
+                        return Err("connection reset by peer".into());
+                    }
+                }
+            }
+        }
+
+        fn parse_frame(&mut self) -> mini_redis::Result<Option<Frame>> {
+            let mut buf = Cursor::new(&self.buffer[..]);
+
+            match Frame::check(&mut buf) {
+                Ok(_) => {
+                    let len = buf.position() as usize;
+
+                    buf.set_position(0);
+
+                    let frame = Frame::parse(&mut buf)?;
+
+                    self.buffer.advance(len);
+
+                    Ok(Some(frame))
+                }
+                Err(Incomplete) => Ok(None),
+                Err(e) => Err(e.into()),
+            }
+        }
+
+        pub async fn write_frame(&mut self, frame: &Frame) -> io::Result<()> {
+            match frame {
+                Frame::Simple(val) => {
+                    self.stream.write_u8(b'+').await?;
+                    self.stream.write_all(val.as_bytes()).await?;
+                    self.stream.write_all(b"\r\n").await?;
+                }
+                Frame::Error(val) => {
+                    self.stream.write_u8(b'-').await?;
+                    self.stream.write_all(val.as_bytes()).await?;
+                    self.stream.write_all(b"\r\n").await?
+                }
+                Frame::Integer(val) => {
+                    self.stream.write_u8(b':').await?;
+                    self.write_decimal(*val).await?;
+                }
+                Frame::Null => {
+                    self.stream.write_all(b"$-1\r\n").await?;
+                }
+                Frame::Bulk(val) => {
+                    let len = val.len();
+
+                    self.stream.write_u8(b'$').await?;
+                    self.write_decimal(len as u64).await?;
+                    self.stream.write_all(val).await?;
+                    self.stream.write_all(b"\r\n").await?;
+                }
+                Frame::Array(_val) => unimplemented!(),
+            }
+
+            self.stream.flush().await?;
+
+            Ok(())
+        }
+
+        async fn write_decimal(&mut self, val: u64) -> io::Result<()> {
+            let mut buf = [0u8; 12];
+            let mut buf = Cursor::new(&mut buf[..]);
+            write!(&mut buf, "{}", val)?;
+
+            let pos = buf.position() as usize;
+            self.stream.write_all(&buf.get_ref()[..pos]).await?;
+            self.stream.write_all(b"\r\n").await?;
+
+            Ok(())
+        }
+    }
 }
