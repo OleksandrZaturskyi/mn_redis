5c1c05620e743ad7d747a5efb518b3104568c47f
aleksandr <zaturskyi.aleksandr@gmail.com>
2022-03-29 15:41:20 +0300

    streams

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
-use tokio::sync::oneshot;
+use mini_redis::client;
+use tokio_stream::StreamExt;
 
 #[tokio::main]
-async fn main() {
-    let (tx1, rx1) = oneshot::channel();
-    let (tx2, rx2) = oneshot::channel();
+async fn main() -> mini_redis::Result<()> {
+    tokio::spawn(async { publish().await });
 
-    tokio::spawn(async {
-        let _ = tx1.send("one");
-    });
+    subscribe().await?;
 
-    tokio::spawn(async {
-        let _ = tx2.send("two");
-    });
+    println!("done");
 
-    tokio::select! {
-        val = rx1 => println!("rx1 completed first with {val:?}"),
-        val = rx2 => println!("rx2 completed first with {val:?}")
-    };
+    Ok(())
+}
+
+async fn publish() -> mini_redis::Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.publish("numbers", "1".into()).await?;
+    client.publish("numbers", "two".into()).await?;
+    client.publish("numbers", "3".into()).await?;
+    client.publish("numbers", "four".into()).await?;
+    client.publish("numbers", "five".into()).await?;
+    client.publish("numbers", "6".into()).await?;
+
+    Ok(())
+}
+
+async fn subscribe() -> mini_redis::Result<()> {
+    let client = client::connect("127.0.0.1:6379").await?;
+    let subscriber = client.subscribe(vec!["numbers".into()]).await?;
+    let messages = subscriber
+        .into_stream()
+        .filter(|msg| match msg {
+            Ok(msg) if msg.content.len() == 1 => true,
+            _ => false,
+        })
+        .map(|msg| msg.unwrap().content)
+        .take(3);
+
+    tokio::pin!(messages);
+
+    while let Some(message) = messages.next().await {
+        println!("GOT = {message:?}")
+    }
+
+    Ok(())
 }
